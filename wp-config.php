452d499ce1f5a2aa2047ffccb2791fb6717fcf75
Kishan <kishan.jaiswal@cygnetinfotech.com>
2023-03-03 16:34:40 +0530

    Menu plugin add

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'cygnetdes' );
+define( 'DB_NAME', 'cygnetdes_v2' );
 
 /** Database username */
 define( 'DB_USER', 'root' ); 
